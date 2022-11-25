69119337afd317a7f95913da2540bac225827cf5
kookong2 <hy010309@naver.com>
2022-11-25 19:41:21 +0900

    Update Ex01.java

--- java/workspace/java_study/src/day12/Ex01.java
+++ java/workspace/java_study/src/day12/Ex01.java
@@ -7,8 +7,10 @@ public class Ex01 {
 		
 		// FileNotFoundException ,, .class 파일 x -> 실행 자체 x
 		try {
-			FileInputStream fis = new FileInputStream("aaa.txt");			
-		}catch (FileNotFoundException e) {
+			FileInputStream fis = new FileInputStream("aaa.txt"); // 실제 파일 x -> 예외 객체를 던지기			
+			System.out.println("실행되는 부분!");
+			
+		}catch (FileNotFoundException e) {	// FileNotFoundException e = new FileNotFoundException(...);
 			System.out.println("예외 발생!");
 		}
 		System.out.println("실행!");

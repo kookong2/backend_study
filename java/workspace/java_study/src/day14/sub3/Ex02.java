b9d27bb7e152b9f9f37825cb41aab33668929a1c
kookong2 <hy010309@naver.com>
2022-11-29 21:58:16 +0900

    .
    .

---
+++ java/workspace/java_study/src/day14/sub3/Ex02.java
@@ -0,0 +1,19 @@
+package day14.sub3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		
+		//System.out.println(date);
+		
+		String pattern = "yyyy-MM-dd HH:mm";
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		String strDate = sdf.format(date);
+		System.out.println(strDate);
+		
+	}
+}

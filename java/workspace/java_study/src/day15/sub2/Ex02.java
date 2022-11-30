6791f5c024ab96fedec81e891b345a905e467ca1
kookong2 <hy010309@naver.com>
2022-11-30 21:55:48 +0900

    .
    .

---
+++ java/workspace/java_study/src/day15/sub2/Ex02.java
@@ -0,0 +1,17 @@
+package day15.sub2;
+
+import java.time.*;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		LocalDate date = LocalDate.now();
+		LocalTime time = LocalTime.now();
+		
+		LocalDateTime lt1 = date.atTime(time);
+		
+		LocalDateTime lt2 = time.atDate(date);
+		
+		LocalDateTime lt3 = LocalDateTime.of(date, time);
+	}
+}

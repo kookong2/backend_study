d86c5f759153268a3f523f5ce80d1b793fde51a2
kookong2 <hy010309@naver.com>
2022-11-30 21:12:11 +0900

    .
    .

---
+++ java/workspace/java_study/src/day15/sub1/Ex01.java
@@ -0,0 +1,30 @@
+package day15.sub1;
+
+import java.util.Calendar;
+import static java.util.Calendar.*;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		
+		//set, get
+		
+		int year = cal.get(YEAR);
+		int month = cal.get(MONTH);	// 0 ~ 11
+		int day = cal.get(DAY_OF_MONTH);
+		
+		System.out.println("year = " + year + ", month = " + (month + 1) + ", day=" + day);
+		
+		int hour = cal.get(HOUR_OF_DAY);	// HOUR 0~11, HOUR_OF_DAY 0~23
+		int min = cal.get(MINUTE);
+		int sec = cal.get(SECOND);
+		
+		System.out.println("hour = " + hour + ", min = " + min + ", sec = " + sec);
+		
+		int yoil = cal.get(DAY_OF_WEEK);
+		System.out.println("yoil = " + yoil);	//1(일요일) ~ 7(토요일)
+		String[] yoils = {"", "일", "월", "화", "수", "목", "금", "토"};
+		System.out.println("요일 = " + yoils[yoil]);
+	}
+}
---
+++ java/workspace/java_study/src/day15/sub1/Ex03.java
@@ -0,0 +1,26 @@
+package day15.sub1;
+
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2022, 1, 28);
+		printDate(cal);
+//		cal.add(Calendar.DAY_OF_MONTH, 1);
+//		printDate(cal);
+		
+		cal.roll(Calendar.DAY_OF_MONTH, 1);
+		printDate(cal);
+	}
+	
+	private static void  printDate(Calendar cal) {
+		Date date = cal.getTime();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(date));
+	}
+}
